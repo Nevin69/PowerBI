663727cc9477585ac730e687664a2030d2ec5a6b
Nevin Abraham <nevinabraham77@gmail.com>
2024-02-07 00:00:16 +0530

    Base Change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/powerbi-portfolio/",
+  base: "https://nevin.github.io/powerbi-portfolio/",
   plugins: [react()],
 })
